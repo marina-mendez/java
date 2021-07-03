e231b33853ddaf37816f9aafee0e20e8bbcae305
Marina Mendez <marina99mendez@gmail.com>
2021-07-03 12:32:14 -0300

    poo

---
+++ src/first/proyect/marina/trips.java
@@ -0,0 +1,18 @@
+package first.proyect.marina;
+
+public class trips {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		viajes info = new viajes(); // Nuestro primer objeto perteneciente a una clase: Instanciar objetos es el proceso de 
+										// generar un ejemplar de una clase, es
+										// decir, la clase es como una declaración de una forma y el objeto es un caso o
+										// elemento concreto que responde a esa forma.
+		
+
+		System.out.println("la informacion sobre su nuevo viaje es  " + info.Cordoba + " en este momento");
+	}
+
+}
+  
